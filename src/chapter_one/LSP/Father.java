61eeafa89c440506df4b436232ea491505fd87ac
shengwen.gan <shengwen.gan@renren-inc.com>
2018-07-12 20:54:41 +0800

    增加.gitignore

---
+++ src/chapter_one/LSP/Father.java
@@ -0,0 +1,23 @@
+package chapter_one.LSP;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by shengwen.gan on 2018/7/5.
+ * desc:
+ */
+public class Father extends People {
+
+	@Override
+	public Collection doSomething (HashMap map) {
+		System.out.println("父类被执行！");
+		return map.values();
+	}
+
+	public Collection doSomething1 (Map map) {
+		System.out.println("父类被执行！");
+		return map.values();
+	}
+}
